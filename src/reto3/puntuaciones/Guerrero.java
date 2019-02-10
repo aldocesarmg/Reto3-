51c9866c701228a723ff89c8a2e7e6ce797f434b
aldocesarmg <aldocesarmg@gmail.com>
2019-02-09 20:46:44 -0600

    Creacion constructores y metodos getter and setter

--- src/reto3/puntuaciones/Guerrero.java
+++ src/reto3/puntuaciones/Guerrero.java
@@ -2,5 +2,40 @@
 package reto3.puntuaciones;
 
 public class Guerrero {
+    private int puntaje;
+    private String nomPersonaje;
+
+    public Guerrero(int puntaje, String nomPersonaje) {
+        this.puntaje = puntaje;
+        this.nomPersonaje = nomPersonaje;
+    }
+
+    public Guerrero(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public Guerrero(String nomPersonaje) {
+        this.nomPersonaje = nomPersonaje;
+    }
+
+    public Guerrero() {
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public String getNomPersonaje() {
+        return nomPersonaje;
+    }
+
+    public void setNomPersonaje(String nomPersonaje) {
+        this.nomPersonaje = nomPersonaje;
+    }
+    
     
 }
--- src/reto3/puntuaciones/Mago.java
+++ src/reto3/puntuaciones/Mago.java
@@ -2,5 +2,40 @@
 package reto3.puntuaciones;
 
 public class Mago {
+    private int puntaje;
+    private String nomPersonaje;
+
+    public Mago(int puntaje, String nomPersonaje) {
+        this.puntaje = puntaje;
+        this.nomPersonaje = nomPersonaje;
+    }
+
+    public Mago(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public Mago(String nomPersonaje) {
+        this.nomPersonaje = nomPersonaje;
+    }
+
+    public Mago() {
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public String getNomPersonaje() {
+        return nomPersonaje;
+    }
+
+    public void setNomPersonaje(String nomPersonaje) {
+        this.nomPersonaje = nomPersonaje;
+    }
+    
     
 }
